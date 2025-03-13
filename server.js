256987738c338711b9743208f752053320b2526a
Jasmine Thomas <jmariet225@yahoo.com>
2025-03-13 15:59:50 -0600

    server file changes

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const httpPort = 80;
 const httpsPort = 443;
-const koa = require( 'koa' );
-const serve = require( 'koa-static' );
-const http = require( 'http' );
-const https = require( 'https' );
-const fs = require( 'fs' );
+import koa from 'koa';
+import serve from 'koa-static';
+import { createServer } from 'http';
+import { createServer as _createServer } from 'https';
+import { readFileSync } from 'fs';
 const app = new koa();
-const cert = fs.readFileSync( '/ssl/cert.crt' );
-const key = fs.readFileSync( '/ssl/private.key' );
+const cert = readFileSync( '/ssl/cert.crt' );
+const key = readFileSync( '/ssl/private.key' );
 
 app.use( serve( __dirname + '/build', {
     maxage: 365 * 24 * 60 * 60
 } ) );
 
-http.createServer( app.callback() ).listen( httpPort, () => console.log( `sever is listening on ${httpPort}` ) );
-https.createServer( { cert, key }, app.callback() ).listen( httpsPort, () => console.log( `sever is listening on ${httpsPort}` ) );
+createServer( app.callback() ).listen( httpPort, () => console.log( `sever is listening on ${httpPort}` ) );
+_createServer( { cert, key }, app.callback() ).listen( httpsPort, () => console.log( `sever is listening on ${httpsPort}` ) );
